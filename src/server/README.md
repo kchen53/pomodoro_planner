b7478bc0ef5adfc03544e49c42d61eb01a47746c
Jacob Ho <jlhxc25@gmail.com>
2023-02-23 16:54:57 -0500

    Changed project structure --  server folder contents changed

---
+++ src/server/README.md
@@ -0,0 +1,15 @@
+# Backend Server
+## How to run server:
+### Choose Angular Handler 
+Backend: Use angular_embedded <br>
+Frontend: Use angular_live <br>
+One should be in src and the other in server folder. Swap them so the relevent handler is in server folder <br>
+
+### Build
+run "go build" in command line (make sure you are in the server folder) <br>
+
+### Run .exe
+run pomodoro_planner.exe <br>
+
+### Server should be running!
+
--- src/server/main.go
+++ src/server/main.go
@@ -1,53 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-
-	//"os"
-	"pomodoro_planner/src/server/utils"
-	//"github.com/gorilla/mux"
 )
 
 func main() {
-	// r := mux.NewRouter()
 
-	// r.HandleFunc("/hello-world", helloWorld)
-
-	// http.Handle("/", r)
-
-	// srv := &http.Server{
-	// 	Handler: r,
-	// 	Addr:    ":" + os.Getenv("PORT"),
-	// }
-
-	// log.Fatal(srv.ListenAndServe())
-
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/hello", helloWorld)
-
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	host := "127.0.0.1:8081"
+	if err := http.ListenAndServe(host, httpHandler()); err != nil {
+		log.Fatalf("Failed to listen on %s: %v", host, err)
 	}
 
-}
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	var data = struct {
-		Title string `json:"title"`
-	}{
-		Title: "Golang + Angular Starter Kit",
-	}
-
-	jsonBytes, err := utils.StructToJSON(data)
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonBytes)
-	return
 }
