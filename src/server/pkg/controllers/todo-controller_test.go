3c7d67476bf5c4150cfb32f0ab458a2d0c6cb246
neyaarajan <neyaarajan@ufl.edu>
2023-03-01 21:07:08 -0500

    test for GetToDoByID

--- src/server/pkg/controllers/todo-controller_test.go
+++ src/server/pkg/controllers/todo-controller_test.go
@@ -1,11 +1,12 @@
 package controllers
 
 import (
-	"github.com/kchen53/pomodoro_planner/pkg/models"
-	"github.com/kchen53/pomodoro_planner/pkg/utils"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/kchen53/pomodoro_planner/pkg/models"
+	"github.com/kchen53/pomodoro_planner/pkg/utils"
 )
 
 // func Test(t *testing.T) {
@@ -61,6 +62,44 @@ func TestGetToDo(t *testing.T) {
 		t.Errorf("Incorrect due date")
 	}
 	if resTodos[0].Complete != todo.Complete {
+		t.Errorf("Incorrect completed state")
+	}
+}
+
+func TestGetToDoByID(t *testing.T) {
+	//Populate array db with a todo
+	var todo models.ToDo
+	todo.ID = 1
+	todo.Task = "Test Name"
+	todo.Due = "Test Day"
+	todo.Complete = true
+	todo.CreateToDo()
+
+	//Create request
+	req, err := http.NewRequest("GET", "/todo", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//Get Response
+	res := httptest.NewRecorder()
+	GetToDoByID(res, req)
+
+	//Parse Response
+	resTodo := models.ToDo{}
+	utils.ParseBodyTest(res, &resTodo)
+
+	//Compare response to input
+	if resTodo.ID != todo.ID {
+		t.Errorf("Incorrect ID")
+	}
+	if resTodo.Task != todo.Task {
+		t.Errorf("Incorrect task name")
+	}
+	if resTodo.Due != todo.Due {
+		t.Errorf("Incorrect due date")
+	}
+	if resTodo.Complete != todo.Complete {
 		t.Errorf("Incorrect completed state")
 	}
 }
