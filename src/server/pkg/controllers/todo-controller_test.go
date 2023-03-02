f4bdb3f912443fcef1ddf267657abbcdf6c02dbb
Jacob Ho <jlhxc25@gmail.com>
2023-03-01 19:50:06 -0500

    First unit test done

--- src/server/pkg/controllers/todo-controller_test.go
+++ src/server/pkg/controllers/todo-controller_test.go
@@ -1,23 +1,66 @@
 package controllers
 
 import (
+	"github.com/kchen53/pomodoro_planner/pkg/models"
+	"github.com/kchen53/pomodoro_planner/pkg/utils"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+// func Test(t *testing.T) {
+// 	req, err := http.NewRequest("GET", "http://example.com/foo", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	res := httptest.NewRecorder()
+// 	GetToDo(res, req)
+
+// 	exp := "Hello World"
+// 	act := res.Body.String()
+// 	if exp != act {
+// 		t.Fatalf("Expected %s gog %s", exp, act)
+// 	}
+// }
+
 func TestGetToDo(t *testing.T) {
-	req, err := http.NewRequest("GET", "http://example.com/foo", nil)
+	//Populate array db with a todo
+	var todo models.ToDo
+	todo.ID = 1
+	todo.Task = "Test Name"
+	todo.Due = "Test Day"
+	todo.Complete = true
+	todo.CreateToDo()
+
+	//Create request
+	req, err := http.NewRequest("POST", "/todo", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	//Get Response
 	res := httptest.NewRecorder()
 	GetToDo(res, req)
 
-	exp := "Hello World"
-	act := res.Body.String()
-	if exp != act {
-		t.Fatalf("Expected %s gog %s", exp, act)
+	//Parse Response
+	resTodos := []models.ToDo{}
+	utils.ParseBodyTest(res, &resTodos)
+
+	//Compare response to input
+	if len(resTodos) != 1 {
+		t.Errorf("Incorrect number of Todo's")
+	}
+	if resTodos[0].ID != todo.ID {
+		t.Errorf("Incorrect ID")
+	}
+	if resTodos[0].Task != todo.Task {
+		t.Errorf("Incorrect task name")
+	}
+	if resTodos[0].Due != todo.Due {
+		t.Errorf("Incorrect due date")
+	}
+	if resTodos[0].Complete != todo.Complete {
+		t.Errorf("Incorrect completed state")
 	}
 }
--- src/server/pkg/utils/utils.go
+++ src/server/pkg/utils/utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 )
 
 func ParseBody(r *http.Request, x interface{}) {
@@ -11,5 +12,11 @@ func ParseBody(r *http.Request, x interface{}) {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
 		}
+	}
+}
+
+func ParseBodyTest(r *httptest.ResponseRecorder, x interface{}) {
+	if err := json.Unmarshal(r.Body.Bytes(), x); err != nil {
+		return
 	}
 }
