acd3fac725e99ae5f3d48eb4e1cf595904aa0a54
Jacob Ho <jlhxc25@gmail.com>
2023-03-22 15:59:21 -0400

    Reformat ToDo, Tests are now invalid and cannot complete todo actions

--- src/server/pkg/models/todo.go
+++ src/server/pkg/models/todo.go
@@ -12,9 +12,10 @@ var db *sql.DB
 var list []ToDo
 
 type ToDo struct {
-	ID       int64  `json:"id"`
-	Task     string `json:"task"`
-	Due      string `json:"due"`
+	Name     string `json:"name"`
+	Date     string `json:"date"` //YYYY-MM-DD
+	Time     int    `json:time`   //seconds
+	Repeat   int8   `json:repeat` //binaryflags: 0:6 = MTWRFSN
 	Complete bool   `json:"complete"`
 }
 
@@ -47,33 +48,33 @@ func GetAllToDo() []ToDo {
 func GetToDoByID(Id int64) (*ToDo, *gorm.DB) {
 	var getToDo ToDo
 	//db := db.Where("ID=?", Id).Find(&getToDo)
-	for _, t := range list {
-		if t.ID == Id {
-			getToDo = t
-		}
-	}
+	// for _, t := range list {
+	// 	if t.Name == Id {
+	// 		getToDo = t
+	// 	}
+	// }
 	return &getToDo, db
 }
 
 func GetToDoByIDList(Id int64) (*ToDo, *[]ToDo) {
 	var getToDo ToDo
-	for _, t := range list {
-		if t.ID == Id {
-			getToDo = t
-		}
-	}
+	// for _, t := range list {
+	// 	if t.ID == Id {
+	// 		getToDo = t
+	// 	}
+	// }
 	return &getToDo, &list
 }
 
 func DeleteToDo(Id int64) ToDo {
 	var todo ToDo
 	//db.Where("ID=?", Id).Delete(todo)
-	for i, t := range list {
-		if t.ID == Id {
-			list = append(list[:i], list[i+1:]...)
-			break
-		}
-	}
+	// for i, t := range list {
+	// 	if t.ID == Id {
+	// 		list = append(list[:i], list[i+1:]...)
+	// 		break
+	// 	}
+	// }
 	return todo
 }
 
