7d3c08ccb9826666b3f9b398f5387d944a799e63
Jacob Ho <jlhxc25@gmail.com>
2023-03-27 16:34:49 -0400

    Changed user route names

--- src/server/pkg/routes/user-routes.go
+++ src/server/pkg/routes/user-routes.go
@@ -7,8 +7,8 @@ import (
 
 // var RegisterBookStoreRoutes = func(router *mux.Router) {
 func RegisterUserRoutes(router *mux.Router) {
-	router.HandleFunc("/user", controllers.LoginUser).Methods("PUT")
-	router.HandleFunc("/user", controllers.SignupUser).Methods("POST")
-	router.HandleFunc("/user", controllers.LogoutUser).Methods("DELETE")
-	router.HandleFunc("/user", controllers.GetUsername).Methods("GET")
+	router.HandleFunc("/login", controllers.LoginUser).Methods("PUT")
+	router.HandleFunc("/signup", controllers.SignupUser).Methods("POST")
+	router.HandleFunc("/signout", controllers.LogoutUser).Methods("PUT")
+	router.HandleFunc("/userdata", controllers.GetUsername).Methods("GET")
 }
