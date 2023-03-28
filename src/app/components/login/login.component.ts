07e0435efdaec4442ffebbdafb2437ff700ec6db
Jacob Pyatt <herobbrine102@gmail.com>
2023-03-28 13:47:13 -0400

    Added http to login and made sign up button

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +10,38 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  mode: string = 'Login';
+  modeButton: string = 'Sign Up';
+  login: boolean = true;
+  success: boolean = false;
+
 
   constructor(
+    private loginService: LoginService,
     public dialogRef: MatDialogRef<LoginComponent>
   ) {  }
 
-  login() {
-    console.log('Username: ' + this.username);
+  onSubmit() {
+    if (this.login) {
+      // login
+      this.loginService.login(this.username, this.password).subscribe(
+        (data) => {
+          console.log(data);
+        });
+    }
+    else {
+      // signup
+      this.loginService.signup(this.username, this.password).subscribe(
+        (data) => {
+          console.log(data);
+        });
+    }
+    this.dialogRef.close();
   }
 
-  onSubmit() {
-    this.dialogRef.close();
+  changeMode() {
+    this.login = !this.login;
+    this.mode = this.login ? 'Login' : 'Sign Up';
+    this.modeButton = this.login ? 'Sign Up' : 'Login';
   }
 }
