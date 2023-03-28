a16a46e91814c5c8cce3b8736eeb0a844161eceb
kchen53 <kellychen@ufl.edu>
2023-03-27 23:32:46 -0400

    added the spotifyApi component files and put the spotifyApi placeholder in the correct place on the session page

--- src/app/components/calendar/calendar.component.spec.ts
+++ src/app/components/calendar/calendar.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AppModule } from 'src/app/app.module';
 import { CalendarComponent } from './calendar.component';
 
 describe('CalendarComponent', () => {
@@ -8,6 +8,9 @@ describe('CalendarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        AppModule
+      ],
       declarations: [ CalendarComponent ]
     })
     .compileComponents();
